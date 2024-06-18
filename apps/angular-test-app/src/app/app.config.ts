5d004f822ae3eef891e5ae8577196120a62812fd
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-06-18 10:48:05 +0200

    feat: format lint angular test app

--- apps/angular-test-app/src/app/app.config.ts
+++ apps/angular-test-app/src/app/app.config.ts
@@ -1,5 +1,7 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import type { ApplicationConfig } from '@angular/core';
+import { provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
