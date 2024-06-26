85692ffe4505b0d4e1c491e871debdd188430eac
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-06-26 14:53:25 +0200

    fix: e2e tests path names, baseURL, playwright

--- apps/angular-test-app-e2e/playwright.config.ts
+++ apps/angular-test-app-e2e/playwright.config.ts
@@ -4,7 +4,8 @@ import { nxE2EPreset } from '@nx/playwright/preset';
 import { defineConfig, devices } from '@playwright/test';
 
 // For CI, you may want to set BASE_URL to the deployed application.
-const baseURL = process.env['BASE_URL'] ?? 'http://localhost:4200';
+const baseURL =
+  process.env['BASE_URL'] ?? 'http://localhost:4200/angular-test-app';
 
 /**
  * Read environment variables from file.
