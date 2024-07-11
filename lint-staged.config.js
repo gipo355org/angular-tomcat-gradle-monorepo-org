83be29247f951d40c0274d5a8107aebc826dcd36
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-07-11 09:12:32 +0200

    WIP remove tsc, fix tsc is not the commad you are looking for

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -4,6 +4,7 @@ module.exports = {
 
   // already in lint affected
   // '*.{ts,tsx,js,jsx,svelte,astro,cjs,mjs,cts,mts,vue}': 'eslint --fix',
+
   // '*.{yml,md,js,ts,tsx,svelte,astro,cjs,mjs,cts,mts,jsx,json,json5,jsonc,css,scss,less,sass,html,htm,mdx,mdown,markdown}':
   // 'prettier --write',
 
